04d577db1f50b55db4a46f65c62ba3d976e64716
ThunderKO <Andy@AndyKo.local>
2017-05-13 11:58:58 +0800

    Facebook Authentication
    +FBLoginManager
    -created the credential
    -enable the read permission
    -enable the sign up
    +FBSDK App Del. sharedInstance
    -open the url in the source application with annotation
    +modify info.plist in the instruction of the Facebook developer web

--- Social/SignInVC.swift
+++ Social/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -19,7 +22,30 @@ class SignInVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Filter: Unable to authenticate with Facebook - \(String(describing: error))")
+            } else if result?.isCancelled == true {
+                print("Filter: User cancelled Facebook Authentication")
+            } else {
+                print("Filter: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
 
-
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("Filter: Unable to authenticate with Facebook - \(String(describing: error))")
+            } else {
+                print("Filter: Successfully authenticated with Firebase")
+            }
+        })
+    }
+    
 }
 
