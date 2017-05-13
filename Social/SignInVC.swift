940679bb5e7f3ac0d59ea64b8b57d1ecdcd54c08
ThunderKO <Andy@AndyKo.local>
2017-05-13 10:23:03 +0800

    Create Own Social Space With FireBase
    +Connect FireBase
    -FB Auth, Storage, Database, Core
    +Login UI
    -Facebook/ Email AC Sign-In

---
+++ Social/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  Social
+//
+//  Created by KO TING on 13/5/2017.
+//  Copyright © 2017年 EdUHK. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
